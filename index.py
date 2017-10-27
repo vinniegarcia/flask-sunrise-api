70b7e958abb82ed4d2c1289cc8433df24a785332
Vinnie Garcia <vinnie@coev.co>
2017-10-27 15:26:46 -0700

    feat(template for index page):

--- index.py
+++ index.py
@@ -1,19 +1,24 @@
 import datetime
 from astral import Astral
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/<string:page_name>/')
+def static_page(page_name = 'index'):
+    return render_template('%s.html' % page_name)
 
 @app.route('/sunrise')
 def get_sunrise():
     a = Astral()
     a.solar_depression = 'civil'
-    city = a['San Francisco']
+    city_candidate = request.args.get('city')
+    city = a[city_candidate]
     print str(city.latitude) + ', ' + str(city.longitude)
     # return city.name
     sun = city.sun(date=datetime.datetime.now().date(), local=True)
     return jsonify(sun)
+
+if __name__ == "__main__":
+    app.run()
