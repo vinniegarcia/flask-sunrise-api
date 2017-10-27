6fe66de678dec5ab1a143c170a11805a86a45ff1
Vinnie Garcia <vinnie@coev.co>
2017-10-26 17:44:26 -0700

    feat(init): initial app - sunrise endpoint using Astral library

---
+++ index.py
@@ -0,0 +1,22 @@
+import datetime
+from astral import Astral
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/sunrise')
+def get_sunrise():
+    a = Astral()
+    a.solar_depression = 'civil'
+    city = a['San Francisco']
+    print str(city.latitude) + ', ' + str(city.longitude)
+    # return city.name
+    sun = city.sun(date=datetime.datetime.now().date(), local=True)
+    return jsonify({
+        'sunset': sun['sunset'],
+        'sunrise': sun['sunrise']
+    })
